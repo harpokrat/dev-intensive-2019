ed987fe99e5b9ba44f07525776ad4704fff7c8b4
svd <svd@xakep.ru>
2019-07-10 20:44:18 +0300

    Реализован метод String.truncate

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,10 @@
+package ru.skillbranch.devintensive.extensions
+
+fun String.truncate(value: Int = 16) : String
+{
+    val string = this.trim()
+    return if (value < string.length)
+        "${string.subSequence(0, value).trim()}..."
+    else
+        string
+}
